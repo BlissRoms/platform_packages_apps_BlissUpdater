2e2f6b94b58167895e29fac264e0cd21af1d6808
Jackeagle <jackeagle102@gmail.com>
2019-03-23 11:23:56 +0530

    BlissUpdater: Fix BATTERY_PLUGGED_ANY Symbol not found Error
    - Add min Java requirements for LAMBDA Functions
    
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- src/com/blissroms/updater/UpdatesListAdapter.java
+++ src/com/blissroms/updater/UpdatesListAdapter.java
@@ -560,7 +560,7 @@ public class UpdatesListAdapter extends RecyclerView.Adapter<UpdatesListAdapter.
         int percent = Math.round(100.f * intent.getIntExtra(BatteryManager.EXTRA_LEVEL, 100) /
                 intent.getIntExtra(BatteryManager.EXTRA_SCALE, 100));
         int plugged = intent.getIntExtra(BatteryManager.EXTRA_PLUGGED, 0);
-        int required = (plugged & BatteryManager.BATTERY_PLUGGED_ANY) != 0 ?
+        int required = (plugged != 0 && (BatteryManager.BATTERY_PLUGGED_USB != 0 || BatteryManager.BATTERY_PLUGGED_AC != 0 || BatteryManager.BATTERY_PLUGGED_WIRELESS != 0))  ?
                 mActivity.getResources().getInteger(R.integer.battery_ok_percentage_charging) :
                 mActivity.getResources().getInteger(R.integer.battery_ok_percentage_discharging);
         return percent >= required;
