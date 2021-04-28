2a2863a9a6ee9d04a361cfb3a60416b75d720962
Jackeagle <jackeagle102@gmail.com>
2021-04-28 04:28:08 -0400

    BlissUpdater: Add Support to specify Bliss Variant param on API URL
    - This will allow to distinguish between Gapps and Non-gapps variants
    
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>
    Change-Id: I29403bbb79e8c145dc2159f44d76b15822a1b86b

--- src/org/lineageos/updater/misc/Constants.java
+++ src/org/lineageos/updater/misc/Constants.java
@@ -46,6 +46,7 @@ public final class Constants {
     public static final String PROP_UPDATER_ALLOW_DOWNGRADING = "bliss.updater.allow_downgrading";
     public static final String PROP_UPDATER_URI = "bliss.updater.uri";
     public static final String PROP_RECOVERY_UPDATE = "persist.sys.recovery_update";
+    public static final String PROP_RELEASE_VARIANT = "ro.bliss.build.variant";
 
     public static final String PREF_INSTALL_OLD_TIMESTAMP = "install_old_timestamp";
     public static final String PREF_INSTALL_NEW_TIMESTAMP = "install_new_timestamp";
