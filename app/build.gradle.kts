516b988bc8ecc5d96717c12cf3585ad1b538ff5f
Jackeagle <jackeagle102@gmail.com>
2023-12-04 10:40:22 +0530

    Refactor for BlissRoms
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,7 +33,7 @@ android {
     compileSdk = 33
 
     defaultConfig {
-        applicationId = "org.lineageos.updater"
+        applicationId = "org.blissroms.updater"
         minSdk = 30
         targetSdk = 33
         versionCode = 1
